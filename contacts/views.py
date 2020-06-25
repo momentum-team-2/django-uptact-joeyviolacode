981198787dee652c1568064c315bc9168afa5fd3
joeyviolacode <joeyviolacode@gmail.com>
2020-06-24 20:49:28 -0400

    All working

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+# from django.core.exceptions import ObjectDoesNotExist
 from .models import Contact
-from .forms import ContactForm
+from .forms import ContactForm, NoteForm
 
 
 # Create your views here.
@@ -21,9 +22,24 @@ def add_contact(request):
 
     return render(request, "contacts/add_contact.html", {"form": form})
 
+def add_note(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    form = NoteForm(data=request.POST)
+    if form.is_valid():
+        note = form.save(commit=False) # don't need this if I can include PK or customer in addition to form
+        note.contact = contact
+        note.save() #necessary?  Had to do it in console, same in code, I assume.
+        return redirect(to='show_contact', pk=pk)
+    return render(request, "contact/show_contact.html", {
+        "note_form": form, 
+        "contact": contact})
+
+
+
 def show_contact(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
-    return render(request, "contacts/show_contact.html", {"contact": contact})
+    form = NoteForm()
+    return render(request, "contacts/show_contact.html", {"contact": contact, "form": form})
 
 
 def edit_contact(request, pk):
--- uptact/urls.py
+++ uptact/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', contacts_views.list_contacts, name='list_contacts'),
     path('contacts/add/', contacts_views.add_contact, name='add_contact'),
     path('contacts/<int:pk>', contacts_views.show_contact, name="show_contact"),
+    path('contacts/<int:pk>/notes/', contacts_views.add_note, name="add_note"),
     path('contacts/<int:pk>/edit/',
          contacts_views.edit_contact,
          name='edit_contact'),
